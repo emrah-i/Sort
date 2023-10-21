4ac3a6f7018916d57047f82dbbda689cc9e115c3
emrah-i <ibraem1026@gmail.com>
2023-10-21 00:45:58 -0400

    Added navbar and range

--- sorting/src/App.jsx
+++ sorting/src/App.jsx
@@ -8,19 +8,54 @@ function Display(props) {
             </div>)
 }
 
+function Nav(props) {
+    const { arrayLength, setArrayLength, setNumbArray } = props
+
+    function changeArray(event) {
+
+        setArrayLength(event.target.value)
+
+        const array = []
+        
+        for (let i = 0; i < arrayLength; i ++) {
+            array.push(generateNumb())
+        }
+
+        setNumbArray(array)
+    }
+
+    return (<nav className="top-nav">
+                <label className="slider-label">{arrayLength}</label>
+                <input className="length-slider" type="range" defaultValue={arrayLength} min="5" max="110" onChange={(event)=>changeArray(event)}></input>
+                <div>
+                    <button className="btn selected">Bubble Sort</button>
+                    <button className="btn">Quick Sort</button>
+                    <button className="btn">Selection Sort</button>
+                    <button className="btn">Insertion Sort</button>
+                </div>
+            </nav>)
+}
+
+function SortBtn() {
+
+    return (<button className="btn sort-btn">Sort</button>)
+}
+
+function generateNumb() {
+    return Math.floor(Math.random() * 651) 
+}
+
 function App(){
 
-    const [numbArray, setNumbArray] = useState([])
+    const [ numbArray, setNumbArray ] = useState([])
+    const [ arrayLength, setArrayLength ] = useState(55)
 
-    function generateNumb() {
-        return Math.floor(Math.random() * 701) 
-    }
 
     useEffect(()=>{
 
         const array = []
         
-        for (let i = 0; i < 100; i ++) {
+        for (let i = 0; i < arrayLength; i ++) {
             array.push(generateNumb())
         }
 
@@ -29,6 +64,8 @@ function App(){
 
 
     return (<div>
+                <Nav arrayLength={arrayLength} setArrayLength={setArrayLength} setNumbArray={setNumbArray} />
+                <SortBtn />
                 <Display numbArray={numbArray} />
             </div>)
 }
