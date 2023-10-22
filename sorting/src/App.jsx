613c8cb97e7a27790b44a60e446135a5e84f7d25
emrah-i <ibraem1026@gmail.com>
2023-10-22 13:55:26 -0400

    Created selection sort

--- sorting/src/App.jsx
+++ sorting/src/App.jsx
@@ -4,73 +4,84 @@ function Display(props) {
     const { numbArray, barsRef } = props
 
     return (<div className="display">
-                {numbArray.map((element, index)=>{return (<p key={index} className="array-bar" style={{height: element}} ref={el => barsRef.current[index] = el} data-height={element} >&nbsp;</p>)})}
+                {numbArray.map((element, index)=>{return (<p key={index} className="array-bar" style={{height: element}} ref={element => barsRef.current[index] = element} data-height={element} >&nbsp;</p>)})}
             </div>)
 }
 
 function Nav(props) {
-    const { arrayLength, setArrayLength } = props
+    const { arrayLength, setArrayLength, setSortType, breakLength, setBreakLength } = props
+    const formRef = useRef(null)
 
     function changeArray(event) {
         setArrayLength(event.target.value)
     }
 
+    function changeBreak(event) {
+        setBreakLength(event.target.value)
+    }
+
+    function changeSort(event) {
+
+        if (event.target.nodeName.toLowerCase() === "button") {
+            if (formRef.current) {
+                formRef.current.childNodes.forEach(element=>{
+                    element.classList.remove("selected")        
+                })
+            }
+
+            event.target.classList.add('selected')
+            setSortType(event.target.dataset.type)
+        }
+    }
+
+
+
     return (<nav className="top-nav">
                 <label className="slider-label">{arrayLength}</label>
                 <input className="length-slider" type="range" defaultValue={arrayLength} min="2" max="110" onChange={(event)=>changeArray(event)}></input>
-                <div>
-                    <button className="btn selected">Bubble Sort</button>
-                    <button className="btn">Quick Sort</button>
-                    <button className="btn">Selection Sort</button>
-                    <button className="btn">Insertion Sort</button>
+                <div className="sort-btns-parent" onClick={(event)=>changeSort(event)} ref={formRef}>
+                    <button className="btn selected" data-type="bubble" >Bubble Sort</button>
+                    <button className="btn" data-type="selection" >Selection Sort</button>
+                    <button className="btn" data-type="quick" >Quick Sort</button>
+                    <button className="btn" data-type="insertion" >Insertion Sort</button>
                 </div>
+                <input className="length-slider" type="range" defaultValue={breakLength} min="0" max="10" step=".5" onChange={(event)=>changeBreak(event)}></input>
+                <label className="time-slider-label">{breakLength} s</label>
             </nav>)
 }
 
-function SortBtn(props) {
+function SortBtns(props) {
 
-    const { numbArray, setNumbArray, barsRef } = props;
+    const { barsRef, generateArray, sortType, bubbleSort, selectionSort } = props;
+    const sortBtns = [useRef(null), useRef(null)]
 
     async function sortItems() {
 
-        let sorting = true;
-        
-        while (sorting) {
+        sortBtns.forEach(element=>{
+            element.current.disabled = true;
+        })
 
-            sorting = false;
-
-            for (let i = 0; i < numbArray.length - 1; i++) {
-
-                barsRef.current.forEach(element=>{
-                    if (element) {
-                        element.classList.remove("selected-bar")
-                }})
-
-                barsRef.current[i].classList.add("selected-bar");
-                if (barsRef.current[i + 1]) {
-                    barsRef.current[i + 1].classList.add("selected-bar");
-                }
-
-                await new Promise(r => setTimeout(r, 10));
-
-                setNumbArray(prevArray => {
-                    const newArray = [...prevArray];
-                        if (i !== newArray.length - 1 && newArray[i] > newArray[i + 1]) {
-                            [newArray[i], newArray[i + 1]] = [newArray[i + 1], newArray[i]];
-                            sorting = true
-                        }
-                    return newArray;
-                });
-            }
+        if (sortType === 'bubble') {
+            await bubbleSort()
+        }
+        else if (sortType === 'selection'){
+            await selectionSort()
         }
 
         barsRef.current.forEach(element=>{
             if (element) {
                 element.classList.add("selected-bar")
         }})
+
+        sortBtns.forEach(element=>{
+            element.current.disabled = false;
+        })
     }
 
-    return (<button className="btn sort-btn" onClick={sortItems}>Sort</button>)
+    return (<div className="sort-btns">
+                <button className="btn sort-btn" onClick={sortItems} ref={sortBtns[0]}>Sort</button>
+                <button className="btn redo-btn" ref={sortBtns[1]} onClick={generateArray} >Randomize</button>
+            </div>)
 }
 
 
@@ -81,32 +92,113 @@ function generateNumb() {
 function App(){
 
     const [ numbArray, setNumbArray ] = useState([])
-    const [ arrayLength, setArrayLength ] = useState(55)
+    const [ sortType, setSortType ] = useState("bubble")
+    const [ arrayLength, setArrayLength ] = useState(25)
+    const [ breakLength, setBreakLength ] = useState(1)
     const barsRef = useRef([])
+    const prevCountRef = useRef(numbArray);
 
-    useEffect(()=>{
+    async function bubbleSort() {
+        let sorting = true;
+            
+        while (sorting) {
+    
+            sorting = false;
+    
+            for (let i = 0; i < numbArray.length - 1; i++) {
+    
+                barsRef.current.forEach(element=>{
+                    if (element) {
+                        element.classList.remove("selected-bar")
+                }})
+    
+                barsRef.current[i].classList.add("selected-bar");
+                if (barsRef.current[i + 1]) {
+                    barsRef.current[i + 1].classList.add("selected-bar");
+                }
+    
+                await new Promise(r => setTimeout(r, (breakLength * 100)));
+    
+                setNumbArray(prevArray => {
+                    const newArray = [...prevArray];
+                        if (i !== newArray.length - 1 && newArray[i] > newArray[i + 1]) {
+                            [newArray[i], newArray[i + 1]] = [newArray[i + 1], newArray[i]];
+                            sorting = true
+                        }
+                    return newArray;
+                });
+            }
+        }
+    }
 
-        const array = []
+    async function selectionSort() {
+            
+        for (let j = 0; j < numbArray.length; j++) {
+
+            const newArray = prevCountRef.current;
+            let min_idx = j;
+            let min = newArray[j];
         
+            for (let i = j + 1; i < numbArray.length; i++) {
+
+                barsRef.current.forEach(element => {
+                    if (element) {
+                        element.classList.remove("selected-bar");
+                    }
+                });
+
+                barsRef.current[i].classList.add("selected-bar");
+
+                await new Promise(r => setTimeout(r, (breakLength * 100)));
+    
+                if (newArray[i] < min) {
+                    min = newArray[i];
+                    min_idx = i;
+                }
+    
+                barsRef.current[i].classList.remove("selected-bar");
+            }
+                
+            setNumbArray(prevArray => {
+                const temp = prevArray[min_idx];
+                prevArray[min_idx] = prevArray[j];
+                prevArray[j] = temp;
+                return [...prevArray];
+            });
+
+            await new Promise(r => setTimeout(r, (breakLength * 100)));
+        }
+    }
+    
+
+    function generateArray() {
+        const array = []
+            
         for (let i = 0; i < arrayLength; i ++) {
             array.push(generateNumb())
         }
-
+    
         setNumbArray(array)
-
-        barsRef.current = []
 
         barsRef.current.forEach(element=>{
             if (element) {
                 element.classList.remove("selected-bar")
         }})
 
+        barsRef.current = []
+    }
+
+    useEffect(()=>{
+        generateArray()
     }, [arrayLength])
 
+    useEffect(() => {
+        prevCountRef.current = numbArray;
+      }, [numbArray]);
 
     return (<div>
-                <Nav arrayLength={arrayLength} setArrayLength={setArrayLength} />
-                <SortBtn numbArray={numbArray} setNumbArray={setNumbArray} barsRef={barsRef} />
+                <Nav arrayLength={arrayLength} setArrayLength={setArrayLength} setSortType={setSortType} breakLength={breakLength} setBreakLength={setBreakLength} />
+                <SortBtns barsRef={barsRef} generateArray={generateArray} sortType={sortType} bubbleSort={bubbleSort} selectionSort={selectionSort} />
                 <Display numbArray={numbArray} barsRef={barsRef} />
             </div>)
 }
