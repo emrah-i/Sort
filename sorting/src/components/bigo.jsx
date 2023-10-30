ddfa490aef15a2e54e0205713c52be0ffb7bc9fc
emrah-i <ibraem1026@gmail.com>
2023-10-29 22:53:49 -0400

    Updated descriptions to each value

--- sorting/src/components/bigo.jsx
+++ sorting/src/components/bigo.jsx
@@ -8,16 +8,26 @@ function BigO(props) {
         content = (
           <div>
             <h2>Bubble Sort:</h2>
-            <p>In bubble sort, we start at a base index of 0 and compare it to the index that directly follows it, 1. We swap the values (if necessary) so the largest value is in the right-most index. 
-                We increment the base index by 1, compare it to the next index, and swap. 
-                We continue incrementing the base index until we reach the second to last index. 
-                At this point, the largest number will have "bubbled" to the top. 
-                We loop through the array n number of times until the array is sorted. 
-                Instead of going to the second to last index each loop, we decrement the length by 1 to not repeatedly sort already sorted values. 
-                If there are no swaps made through one full loop, the function immediately returns. This eliminates unnecessary sorting.</p>
-            <p><b>Best-case:</b> O(n) –– when the list is already sorted (function returns in no swaps made).</p>
-            <p><b>Average-case:</b> O(n<sup>2</sup>) –– since you have to loop through each index n number of times.</p>
-            <p><b>Worst-case:</b> O(n<sup>2</sup>) –– when the list is reversed.</p>
+            <p>
+              Bubble sort is a comparison-based sorting algorithm that works by iteratively moving the largest unsorted value to the right-most position in the array, similar to how bubbles rise to the surface in a liquid.
+            </p>
+            <p>
+              We start by setting the first index (index 0) as our base index. Then, we compare its value to the index to the right of it (index 1).
+              If the value at the base index is greater than the one right of it, they are swapped. 
+              This process continues, with the base index incrementing by one each time, until we reach the second to last index. 
+              By the end of the first loop, the largest number will have "bubbled up" to the last index.
+            </p>
+            <p>
+              For the next loop, we don't need to consider the last index since it's already in its correct place. 
+              Thus, with each successive pass through the array, we decrement the effective size of the array to sort by 1. 
+            </p>
+            <p>
+              An optimization has been added to my algorithm: If during a pass, no swaps are made, then the array is already sorted and the algorithm can terminate early, avoiding unnecessary comparisons.
+            </p>
+
+            <p><b>Best-case:</b> O(n) –– when the array is already sorted.</p>
+            <p><b>Average-case:</b> O(n<sup>2</sup>) –– you have to loop through each index n number of times.</p>
+            <p><b>Worst-case:</b> O(n<sup>2</sup>) –– when the array is reversed.</p>
           </div>
         );
         break;
@@ -26,12 +36,24 @@ function BigO(props) {
         content = (
           <div>
             <h2>Selection Sort:</h2>
-            <p>In selection sort, we start at a base index of 0, loop through the whole array starting at the base index, find the lowest number, and swap it with the base index.
-                We increment the base index by 1 and repeat. We continue this until we've reached the second to last index. Essentially, all of the smallest numbers are "selected"
-                and placed at the bottom. The downfall of this algorithm is that you must loop through each index n number of times even if the list is sorted.
-                This is because even if the first 23 elements are sorted, without the base index getting to the second to last element, you cannot know if the list is sorted.
+            <p>
+              Selection sort is a comparison-based sorting algorithm that works by repeatedly selecting the smallest unsorted value and placing it in its correct position. 
             </p>
-            <p><b>Best-case:</b> O(n<sup>2</sup>) –– even if the list is sorted, you have to increment through the whole array.</p>
+            <p>
+              Starting from the base index (index 0), we search for the smallest value in the array (including the base index). 
+              Once found, we swap this value with the value at the base index. 
+              Next, we increment the base index by 1 and again search for the smallest value in the remaining unsorted portion, then swap it with the base index. 
+              Remember, each loop starts at the base index since only the portion from this index on is unsorted.
+              This process continues until we reach the second to last index of the array. 
+              By the end of each loop, the smallest number from the unsorted portion is "selected" and placed in its correct position.
+            </p>
+            <p>
+              A limitation of selection sort is that it performs the same number of comparisons even if the list is fully sorted. 
+              For example, if the first 23 out of 25 elements are sorted, the algorithm has no way to know until it reaches the second to last index.
+              This is because during each loop it is only selecting the smallest number. If the base index is the smallest number, it simply increments by 1 and continues.
+              Therefore, even if the all elements are sorted, the algorithm must still proceed until it has inspected all elements to guarantee the array's sorted state.
+            </p>
+            <p><b>Best-case:</b> O(n<sup>2</sup>) –– even if the array is sorted, you have to loop through the whole array n number of times.</p>
             <p><b>Average-case:</b> O(n<sup>2</sup>) –– since you have to loop through each index n number of times.</p>
             <p><b>Worst-case:</b> O(n<sup>2</sup>)</p>
           </div>
@@ -42,14 +64,28 @@ function BigO(props) {
         content = (
           <div>
             <h2>Insertion Sort:</h2>
-            <p>In insertion sort, we start at a base index of 1, compare it with the index directly to the left, and swap (if necessary) so the smaller number is in the left-most index.
-                Then, we increment the base index by 1, however, this time, we keep comparing and swapping with indexes to the left until we find a value that's less than the base value.
-                This means we've found it's place in the index. We can be sure of this because its below greater values and above lesser values. Essentially, we're inserting numbers 
-                to the proper index by finding the index where the number to the left is less than and the number to the right is greater than our base value. 
+            <p>
+            Insertion sort is a comparison-based sorting algorithm that builds the sorted portion of an array one element at a time. 
             </p>
-            <p><b>Best-case:</b> O(n) –– when the list is already sorted.</p>
+            <p>
+              We start by setting the second element (index 1) as our base index. Then, we compare it to all preceding elements (all lower indexes). 
+              In this case, we only have the element directly to its left (index 0). 
+              If the base value is smaller, we swap the two. 
+            </p>
+            <p>
+              With each interation, we increment the base index by 1 and compare it again to all preceding elements.
+              These comparisons occur one at a time. If the base index is smaller, a swap occurs. 
+              The base value does not change until the value preceding it is smaller or it has reached the index 0.
+              Once one of these two events occur, the base index is incremented by one.
+              Remember, this incrementation occurs from the original index of the base, not the current index of the base value.
+            </p>
+            <p>
+             The beauty of insertion sort lies in its ability to quickly sort a partially sorted array. 
+             As the base element finds its correct position, the elements to its left are always in a sorted state.
+            </p>
+            <p><b>Best-case:</b> O(n) –– when the array is already sorted.</p>
             <p><b>Average-case:</b> O(n<sup>2</sup>)</p>
-            <p><b>Worst-case:</b> O(n<sup>2</sup>) –– when the list is reversed.</p>
+            <p><b>Worst-case:</b> O(n<sup>2</sup>) –– when the array is reversed.</p>
           </div>
         );
         break;
@@ -58,26 +94,29 @@ function BigO(props) {
         content = (
           <div>
             <h2>Quick Sort:</h2>
-            <p>In quick sort, there is no concrete way to start. My algorithm uses the last index, however, this is not efficient 
-                as the starting pivot index should be close to the median of the array. I will only explain how my algorithm works. 
-                This will be a little more complicated than the other algorithms as it contains recursion.</p>
-            <p>For my quick sort, you start at the last index, called the pivot index. A comparison index starts at the index 0.
-                This comparison index increments by 1 until it finds a value greater than the pivot value. 
-                Once this value is found, a loop starts at this index and increments by 1 until it finds
-                a value lesser than the pivot. We will call this loop index the swap index. 
-                When this lesser value is found, the swap index and comparison index swap. 
-                The comparison index increments by 1 and the swap index continues from the previous swap index where the swap took place. 
-                Once the swap index reaches the pivot index, the comparison index is swapped with the pivot index. 
-                This might not make sense, but remember, we were always comparing the swap index to the pivot index, never the comparison index.
-                Therefore, this array is sorted according the pivot.</p>
-            <p>Following this is where recursion is utilized. After the final swap, we have a new pivot index. 
-                Now, we sort everything to the left and everything to the right of the pivot (seperately) using the same method described above.
-                The only thing that changes is the start and end points of the array. 
-                We do this by changing the start and end parameters on the function call.
+            <p>
+              Quicksort is a divide-and-conquer sorting algorithm, and there are multiple ways to implement it. 
+              My version chooses the last element as the pivot, though ideally, the pivot should be near the median for optimal performance. 
+              This explanation covers my specific approach, which incorporates recursion.
             </p>
-            <p><b>Best-case:</b> O(n • logn) –– with a good pivot selection.</p>
-            <p><b>Average-case:</b> O(n • logn)</p>
-            <p><b>Worst-case:</b> O(n<sup>2</sup>) –– when the pivot is always the smallest or largest element.</p>
+            <p>
+              In my algorithm, the pivot is the last element. 
+              We also have a 'comparison' index starting from the beginning of the array. 
+              It moves forward until it encounters a value greater than the pivot. 
+              Upon finding such a value, another 'swap' index starts from this position and increments until it finds a value less than the pivot. 
+              Once found, the values at 'comparison' and 'swap' indices are exchanged. 
+              The 'comparison' index moves one step, and the 'swap' index continues its journey from its last position. 
+              This process ensures we're always comparing the 'swap' index's value against the pivot. 
+              Once the 'swap' index meets the pivot, we swap the 'comparison' index value with the pivot, effectively placing the pivot in its sorted position.
+            </p>
+            <p>
+              After positioning the pivot, the array gets divided into two parts: left and right of the pivot. 
+              Recursion comes into play here. 
+              We apply the same sorting method separately to both sub-arrays by adjusting the start and end parameters in the recursive function call.
+            </p>
+            <p><b>Best-case:</b> O(n log n) –– with a good pivot selection.</p>
+            <p><b>Average-case:</b> O(n log n)</p>
+            <p><b>Worst-case:</b> O(n<sup>2</sup>) –– when the pivot is always the smallest or largest element. For my algorithm, this would be when the array is sorted.</p>
           </div>
         );
         break;
--- sorting/src/components/information.jsx
+++ sorting/src/components/information.jsx
@@ -3,16 +3,26 @@ import BigO from "./bigo";
 
 function Information(props) {
     const { sortType } = props;
-    const [ bigO, setBigO ] = useState(null)
 
     return (<div className="information">
                 <div>
                     <h2>Definitions</h2>
-                    <p><b>Worst Case:</b> &nbsp;The time complexity when the input is in the least favorable condition for the algorithm. For example, if the array is in complete reverse order.</p>
-                    <p><b>Best Case:</b> &nbsp;The time complexity when the input is in the most favorable condition for the algorithm. For example, if the array is already sorted.</p>
+                    <p><b>Worst Case:</b> &nbsp;The time complexity when the input is in the least favorable condition for the algorithm.</p>
+                    <p><b>Best Case:</b> &nbsp;The time complexity when the input is in the most favorable condition for the algorithm.</p>
                     <p><b>Average Case:</b>  &nbsp;Takes into account all possible input configurations and their likelihood.</p>
                     <p><b>n:</b> &nbsp;Input size or, in this case, the number of elements in the array.</p>
                     <p><b>Big O Notation:</b> &nbsp;A tool used to describe the time complexity relative to n. To simplify, the time it takes to sort the array relative to n.</p>
+                    <p><b>Base Index:</b> &nbsp;The index used to compare the other values to (personal definition).</p>
+                    <b>Best Time Complexity</b>
+                    <ul>
+                        <li>O(n)</li>
+                        <li>O(log n)</li>
+                        <li>O(n)</li>
+                        <li>O(n log n)</li>
+                        <li>O(n<sup>2</sup>)</li>
+
+                    </ul>
+                    <b>Worst Time Complexity</b>
                 </div>
                 <div>
                     <BigO sortType={sortType} />
